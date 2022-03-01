b8e65c8a55553a95bff7b64071d51a618b24822f
cbj <zmz2333@qq.com>
2022-03-01 15:56:06 +0800

    1.1.6

--- app/src/main/java/com/cbj/aresandroid/MainActivity.kt
+++ app/src/main/java/com/cbj/aresandroid/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.cbj.aresandroid
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import android.widget.Button
 import com.alibaba.android.arouter.launcher.ARouter
 import com.cbj.aresandroid.databinding.ActivityMainBinding
 import com.cbj.sdk.libui.BaseActivity
--- app/src/main/java/com/cbj/aresandroid/demo/ui/DemoActivity.kt
+++ app/src/main/java/com/cbj/aresandroid/demo/ui/DemoActivity.kt
@@ -1,11 +1,8 @@
 package com.cbj.aresandroid.demo.ui
 
-import android.util.Log
 import android.view.View
-import android.widget.TextView
 import androidx.activity.viewModels
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.cbj.aresandroid.R
 import com.cbj.aresandroid.databinding.ActivityDemoBinding
 import com.cbj.sdk.libui.*
 import dagger.hilt.android.AndroidEntryPoint
@@ -28,7 +25,7 @@ class DemoActivity:BaseActivity() {
 
     override fun initView() {
         initObserver()
-        mBinding.btn.clickDelay {
+        mBinding.btn.singleClick {
             mModule.login()
         }
 
